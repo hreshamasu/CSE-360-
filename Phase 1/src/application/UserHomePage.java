b0ade365c67df6a57660ecd7bd0e2ad2c6678590
Connor Ford <connor4ford@gmail.com>
2025-02-06 18:02:18 -0700

    Update UserHomePage.java
    Added logout button and support

--- Phase 1/src/application/UserHomePage.java
+++ Phase 1/src/application/UserHomePage.java
@@ -1,9 +1,12 @@
 package application;
 
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import databasePart1.*;
 
 /**
  * This page displays a simple welcome message for the user.
@@ -11,15 +14,28 @@ import javafx.stage.Stage;
 
 public class UserHomePage {
 
-    public void show(Stage primaryStage) {
+    public void show(DatabaseHelper databaseHelper, Stage primaryStage) {
     	VBox layout = new VBox();
 	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
 	    
 	    // Label to display Hello user
 	    Label userLabel = new Label("Hello, User!");
 	    userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+	    
+	    // Button to logout and navigate back to login page
+	    Button logoutButton = new Button("Logout");
+	    logoutButton.setOnAction(a -> {
+	    	new SetupLoginSelectionPage(databaseHelper).show(primaryStage);
+	    });
+	    
+	    // Button to quit the application
+	    Button quitButton = new Button("Quit");
+	    quitButton.setOnAction(a -> {
+	    	databaseHelper.closeConnection();
+	    	Platform.exit(); // Exit the JavaFX application
+	    });
 
-	    layout.getChildren().add(userLabel);
+	    layout.getChildren().addAll(userLabel, logoutButton, quitButton);
 	    Scene userScene = new Scene(layout, 800, 400);
 
 	    // Set the scene to primary stage
