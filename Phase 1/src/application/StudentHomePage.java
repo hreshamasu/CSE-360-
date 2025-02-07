b8dc7e114c539c7b660d760acf76c32cf75bbb3f
Ricardo Arreola Bojorquez <91919208+RichyLU05@users.noreply.github.com>
2025-02-07 00:06:13 -0700

    Create StudentHomePage.java

---
+++ Phase 1/src/application/StudentHomePage.java
@@ -0,0 +1,46 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import databasePart1.*;
+
+/**
+ * This page displays a welcome message for the Student.
+ */
+
+public class StudentHomePage {
+
+    public void show(DatabaseHelper databaseHelper, Stage primaryStage) {
+    	VBox layout = new VBox();
+	    layout.setStyle("-fx-alignment: center; -fx-padding: 20;");
+	    
+	    // Label to display Hello user
+	    Label userLabel = new Label("Hello, Student!");
+	    userLabel.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+	    
+	    // Button to logout and navigate back to login page
+	    Button logoutButton = new Button("Logout");
+	    logoutButton.setOnAction(a -> {
+	    	new SetupLoginSelectionPage(databaseHelper).show(primaryStage);
+	    });
+	    
+	    // Button to quit the application
+	    Button quitButton = new Button("Quit");
+	    quitButton.setOnAction(a -> {
+	    	databaseHelper.closeConnection();
+	    	Platform.exit(); // Exit the JavaFX application
+	    });
+
+	    layout.getChildren().addAll(userLabel, logoutButton, quitButton);
+	    Scene userScene = new Scene(layout, 800, 400);
+
+	    // Set the scene to primary stage
+	    primaryStage.setScene(userScene);
+	    primaryStage.setTitle("Student Page");
+    	
+    }
+}
