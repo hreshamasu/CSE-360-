d545f23b69ac9463fc494f4e03c3823baeaaf6fa
Shravan Karande <162378512+skarande3@users.noreply.github.com>
2025-02-28 03:50:31 -0700

    Update Question.java

--- Phase 2/src/application/Question.java
+++ Phase 2/src/application/Question.java
@@ -7,6 +7,9 @@ public class Question {
 	
 	// Constructor to initialize a question with a user name, title, and body
 	public Question(String userName, String title, String body) {
+		 if (title.length() > 255) {
+	            throw new IllegalArgumentException("Titles must be shorter than 255 characters");
+	        }
 		this.userName = userName;
 		this.title = title;
 		this.body = body;
